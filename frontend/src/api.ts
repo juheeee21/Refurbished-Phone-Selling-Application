eb94e06c3b74c0165330a762074cb3b003538404
Juhi Kumari <100509225+juheeee21@users.noreply.github.com>
2025-08-16 15:12:12 +0530

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://refurbished-phone-selling-application.onrender.com",
 });
 
 export const setAuthToken = (token: string) => {
